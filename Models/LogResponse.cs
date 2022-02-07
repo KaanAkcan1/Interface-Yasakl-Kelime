0354f7accd30db7681d7c9d892cf77db2a07b153
Kaan Akcan <kaan.akcan@performans.com>
2022-02-07 15:48:37 +0300

    Add project files.

---
+++ Models/LogResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Interface.Models
+{
+    public class LogResponse
+    {
+        public Log Data { get; set; }
+        public string Message { get; set; }
+        public bool Success { get; set; }
+    }
+}
---
+++ Models/StringResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Interface.Models
+{
+    public class StringResponse
+    {
+        public string[] Data { get; set; }
+        public string Message { get; set; }
+        public bool Success { get; set; }
+    }
+}
